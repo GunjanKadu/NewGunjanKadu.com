0363197916a018fc418a452826ef555e7d673d0f
Gunjan Kadu <agunjan.kadu@gmail.com>
2021-09-27 16:48:18 +0200

    without cookies

--- components/global/cookie-box.vue
+++ components/global/cookie-box.vue
@@ -1,45 +1,45 @@
 <template lang="pug">
-vue-if-bot
-  cookie-consent.d-flex.flex-column.flex-md-row.pa-3.shadow-md(
-    transition='scale-transition'
-  )
-    template(#message)
-      .white--text
-        | {{ $t("app.cookie.message") }}&nbsp;
-        a.primary--text.text-capitalize(
-          href='https://cookiesandyou.com/',
-          target='_blank',
-          rel='noopener noreferrer'
-        )
-          | {{ $t("more.details") }}
-          v-icon(right, small, color='primary') {{ mdiOpenInNew }}
-    template(#button)
-      v-btn.btn.btn-info.mt-2.mt-md-0.ml-md-2(
-        :block='$vuetify.breakpoint.xs',
-        large,
-        color='primary'
-      )
-        | Ok
+//- vue-if-bot
+//-   cookie-consent.d-flex.flex-column.flex-md-row.pa-3.shadow-md(
+//-     transition='scale-transition'
+//-   )
+//-     template(#message)
+//-       .white--text
+//-         | {{ $t("app.cookie.message") }}&nbsp;
+//-         a.primary--text.text-capitalize(
+//-           href='https://cookiesandyou.com/',
+//-           target='_blank',
+//-           rel='noopener noreferrer'
+//-         )
+//-           | {{ $t("more.details") }}
+//-           v-icon(right, small, color='primary') {{ mdiOpenInNew }}
+//-     template(#button)
+//-       v-btn.btn.btn-info.mt-2.mt-md-0.ml-md-2(
+//-         :block='$vuetify.breakpoint.xs',
+//-         large,
+//-         color='primary'
+//-       )
+//-         | Ok
 </template>
 
 <script>
-import { mdiOpenInNew } from '@mdi/js'
+// import { mdiOpenInNew } from '@mdi/js'
 
-export default {
-  name: 'CookieBox',
-  components: {
-    VueIfBot: () => import('vue-if-bot'),
-  },
-  data() {
-    return {
-      mdiOpenInNew,
-    }
-  },
-}
+// export default {
+//   name: 'CookieBox',
+//   components: {
+//     VueIfBot: () => import('vue-if-bot'),
+//   },
+//   data() {
+//     return {
+//       mdiOpenInNew,
+//     }
+//   },
+// }
 </script>
 
 <style>
-.cookie-consent {
+/* .cookie-consent {
   position: fixed;
   right: 0.75rem;
   bottom: 0.75rem;
@@ -55,5 +55,5 @@ export default {
     right: 0.25rem;
     left: 0.25rem;
   }
-}
+} */
 </style>
